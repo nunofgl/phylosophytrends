343dba1bc47f4aa6e8c5a6b4faf4092004db6ca8
Nuno Lages <nunofgl@gmail.com>
2019-02-03 20:37:05 +0000

    renamed app and package

--- src/main/java/net/nunolages/phylosophytrends/PhilosophyTrendsApplication.java
+++ src/main/java/net/nunolages/phylosophytrends/PhilosophyTrendsApplication.java
@@ -1,4 +1,4 @@
-package com.example.phylosophytrends;
+package net.nunolages.phylosophytrends;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
-public class DemoApplication {
+public class PhilosophyTrendsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PhilosophyTrendsApplication.class, args);
 	}
 
 	@RequestMapping("/")

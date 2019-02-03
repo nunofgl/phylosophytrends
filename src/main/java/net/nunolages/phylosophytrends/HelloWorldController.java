c59019182057738ff41795cf0929456fe4330c4e
Nuno Lages <nunofgl@gmail.com>
2019-02-03 20:47:52 +0000

    separated hello world from main app

---
+++ src/main/java/net/nunolages/phylosophytrends/HelloWorldController.java
@@ -0,0 +1,16 @@
+package net.nunolages.phylosophytrends;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+
+}

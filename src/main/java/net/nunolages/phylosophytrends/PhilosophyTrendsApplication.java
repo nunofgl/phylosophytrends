025c5688b5f39ea8dfdfde5bcf5e510c04700f22
Nuno Lages <nunofgl@gmail.com>
2019-02-03 20:47:52 +0000

    separated hello world from main app

--- src/main/java/net/nunolages/phylosophytrends/PhilosophyTrendsApplication.java
+++ src/main/java/net/nunolages/phylosophytrends/PhilosophyTrendsApplication.java
@@ -2,11 +2,7 @@ package net.nunolages.phylosophytrends;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication
 public class PhilosophyTrendsApplication {
 
@@ -14,10 +10,5 @@ public class PhilosophyTrendsApplication {
 		SpringApplication.run(PhilosophyTrendsApplication.class, args);
 	}
 
-	@RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
 }
 

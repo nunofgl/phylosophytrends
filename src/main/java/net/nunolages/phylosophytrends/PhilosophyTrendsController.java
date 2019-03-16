1504790ecdc70dd299bd6e906a9932e74294bdf6
nuno <nuno@hermes.lan>
2019-03-16 22:46:05 +0000

    Clean up.

---
+++ src/main/java/net/nunolages/phylosophytrends/PhilosophyTrendsController.java
@@ -0,0 +1,16 @@
+package net.nunolages.phylosophytrends;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PhilosophyTrendsController {
+
+	@GetMapping(value = { "/" })
+	public ModelAndView app() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("app");
+		return model;
+	}
+}
